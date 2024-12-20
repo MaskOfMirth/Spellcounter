f0b3e35377c7ebd6384bd52ee1f33fea4fb57691
MaskOfMirth <97659932+MaskOfMirth@users.noreply.github.com>
2024-12-20 11:50:42 +0100

    SPLLArray+ButtonsMitFor

--- src/app/spell-per-level/spell-per-level.component.ts
+++ src/app/spell-per-level/spell-per-level.component.ts
@@ -13,176 +13,173 @@ export class SpellPerLevelComponent {
   spellPerLevelList = [
     {
       classLevel: 1,
-      spellLevel1: 2
+      spellLevel1: ["Slot 1", "Slot 2"]
     },
     {
       classLevel: 2,
-      spellLevel1: 3,
+      spellLevel1: ["Slot 1", "Slot 2", "Slot 3"]
     },
     {
       classLevel: 3,
-      spellLevel1: 4,
-      spellLevel2: 2
+      spellLevel1: ["Slot 1", "Slot 2", "Slot 3", "Slot 4"],
+      spellLevel2: ["Slot 1", "Slot 2"]
     },
     {
       classLevel: 4,
-      spellLevel1: 4,
-      spellLevel2: 3
+      spellLevel1: ["Slot 1", "Slot 2", "Slot 3", "Slot 4"],
+      spellLevel2: ["Slot 1", "Slot 2", "Slot 3"]
     },
     {
       classLevel: 5,
-      spellLevel1: 4,
-      spellLevel2: 3,
-      spellLevel3: 2
+      spellLevel1: ["Slot 1", "Slot 2", "Slot 3", "Slot 4"],
+      spellLevel2: ["Slot 1", "Slot 2", "Slot 3"],
+      spellLevel3: ["Slot 1", "Slot 2"]
     },
     {
       classLevel: 6,
-      spellLevel1: 4,
-      spellLevel2: 3,
-      spellLevel3: 3
+      spellLevel1: ["Slot 1", "Slot 2", "Slot 3", "Slot 4"],
+      spellLevel2: ["Slot 1", "Slot 2", "Slot 3"],
+      spellLevel3: ["Slot 1", "Slot 2", "Slot 3"]
     },
     {
       classLevel: 7,
-      spellLevel1: 4,
-      spellLevel2: 3,
-      spellLevel3: 3,
-      spellLevel4: 1
+      spellLevel1: ["Slot 1", "Slot 2", "Slot 3", "Slot 4"],
+      spellLevel2: ["Slot 1", "Slot 2", "Slot 3"],
+      spellLevel3: ["Slot 1", "Slot 2", "Slot 3"],
+      spellLevel4: ["Slot 1"]
     },
     {
       classLevel: 8,
-      spellLevel1: 4,
-      spellLevel2: 3,
-      spellLevel3: 3,
-      spellLevel4: 2
+      spellLevel1: ["Slot 1", "Slot 2", "Slot 3", "Slot 4"],
+      spellLevel2: ["Slot 1", "Slot 2", "Slot 3"],
+      spellLevel3: ["Slot 1", "Slot 2", "Slot 3"],
+      spellLevel4: ["Slot 1", "Slot 2"]
     },
     {
       classLevel: 9,
-      spellLevel1: 4,
-      spellLevel2: 3,
-      spellLevel3: 3,
-      spellLevel4: 3,
-      spellLevel5: 1
+      spellLevel1: ["Slot 1", "Slot 2", "Slot 3", "Slot 4"],
+      spellLevel2: ["Slot 1", "Slot 2", "Slot 3"],
+      spellLevel3: ["Slot 1", "Slot 2", "Slot 3"],
+      spellLevel4: ["Slot 1", "Slot 2", "Slot 3"],
+      spellLevel5: ["Slot 1"]
     },
     {
       classLevel: 10,
-      spellLevel1: 4,
-      spellLevel2: 3,
-      spellLevel3: 3,
-      spellLevel4: 3,
-      spellLevel5: 2
+      spellLevel1: ["Slot 1", "Slot 2", "Slot 3", "Slot 4"],
+      spellLevel2: ["Slot 1", "Slot 2", "Slot 3"],
+      spellLevel3: ["Slot 1", "Slot 2", "Slot 3"],
+      spellLevel4: ["Slot 1", "Slot 2", "Slot 3"],
+      spellLevel5: ["Slot 1", "Slot 2"]
     },
     {
       classLevel: 11,
-      spellLevel1: 4,
-      spellLevel2: 3,
-      spellLevel3: 3,
-      spellLevel4: 3,
-      spellLevel5: 2,
-      spellLevel6: 1
+      spellLevel1: ["Slot 1", "Slot 2", "Slot 3", "Slot 4"],
+      spellLevel2: ["Slot 1", "Slot 2", "Slot 3"],
+      spellLevel3: ["Slot 1", "Slot 2", "Slot 3"],
+      spellLevel4: ["Slot 1", "Slot 2", "Slot 3"],
+      spellLevel5: ["Slot 1", "Slot 2"],
+      spellLevel6: ["Slot 1"]
     },
     {
       classLevel: 12,
-      spellLevel1: 4,
-      spellLevel2: 3,
-      spellLevel3: 3,
-      spellLevel4: 3,
-      spellLevel5: 2,
-      spellLevel6: 1
+      spellLevel1: ["Slot 1", "Slot 2", "Slot 3", "Slot 4"],
+      spellLevel2: ["Slot 1", "Slot 2", "Slot 3"],
+      spellLevel3: ["Slot 1", "Slot 2", "Slot 3"],
+      spellLevel4: ["Slot 1", "Slot 2", "Slot 3"],
+      spellLevel5: ["Slot 1", "Slot 2"],
+      spellLevel6: ["Slot 1"]
     },
     {
       classLevel: 13,
-      spellLevel1: 4,
-      spellLevel2: 3,
-      spellLevel3: 3,
-      spellLevel4: 3,
-      spellLevel5: 2,
-      spellLevel6: 1,
-      spellLevel7: 1
+      spellLevel1: ["Slot 1", "Slot 2", "Slot 3", "Slot 4"],
+      spellLevel2: ["Slot 1", "Slot 2", "Slot 3"],
+      spellLevel3: ["Slot 1", "Slot 2", "Slot 3"],
+      spellLevel4: ["Slot 1", "Slot 2", "Slot 3"],
+      spellLevel5: ["Slot 1", "Slot 2"],
+      spellLevel6: ["Slot 1"],
+      spellLevel7: ["Slot 1"]
     },
     {
       classLevel: 14,
-      spellLevel1: 4,
-      spellLevel2: 3,
-      spellLevel3: 3,
-      spellLevel4: 3,
-      spellLevel5: 2,
-      spellLevel6: 1,
-      spellLevel7: 1
+      spellLevel1: ["Slot 1", "Slot 2", "Slot 3", "Slot 4"],
+      spellLevel2: ["Slot 1", "Slot 2", "Slot 3"],
+      spellLevel3: ["Slot 1", "Slot 2", "Slot 3"],
+      spellLevel4: ["Slot 1", "Slot 2", "Slot 3"],
+      spellLevel5: ["Slot 1", "Slot 2"],
+      spellLevel6: ["Slot 1"],
+      spellLevel7: ["Slot 1"]
     },
     {
       classLevel: 15,
-      spellLevel1: 4,
-      spellLevel2: 3,
-      spellLevel3: 3,
-      spellLevel4: 3,
-      spellLevel5: 2,
-      spellLevel6: 1,
-      spellLevel7: 1,
-      spellLevel8: 1
+      spellLevel1: ["Slot 1", "Slot 2", "Slot 3", "Slot 4"],
+      spellLevel2: ["Slot 1", "Slot 2", "Slot 3"],
+      spellLevel3: ["Slot 1", "Slot 2", "Slot 3"],
+      spellLevel4: ["Slot 1", "Slot 2", "Slot 3"],
+      spellLevel5: ["Slot 1", "Slot 2"],
+      spellLevel6: ["Slot 1"],
+      spellLevel7: ["Slot 1"],
+      spellLevel8: ["Slot 1"]
 
     },
     {
       classLevel: 16,
-      spellLevel1: ['R', 'R', 'R', 'R'],
-      spellLevel2: 3,
-      spellLevel3: 3,
-      spellLevel4: 3,
-      spellLevel5: 2,
-      spellLevel6: 1,
-      spellLevel7: 1,
-      spellLevel8: 1
+      spellLevel1: ["Slot 1", "Slot 2", "Slot 3", "Slot 4"],
+      spellLevel2: ["Slot 1", "Slot 2", "Slot 3"],
+      spellLevel3: ["Slot 1", "Slot 2", "Slot 3"],
+      spellLevel4: ["Slot 1", "Slot 2", "Slot 3"],
+      spellLevel5: ["Slot 1", "Slot 2"],
+      spellLevel6: ["Slot 1"],
+      spellLevel7: ["Slot 1"],
+      spellLevel8: ["Slot 1"]
     },
     {
       classLevel: 17,
-      spellLevel1: 4,
-      spellLevel2: 3,
-      spellLevel3: 3,
-      spellLevel4: 3,
-      spellLevel5: 2,
-      spellLevel6: 1,
-      spellLevel7: 1,
-      spellLevel8: 1,
-      spellLevel9: 1
+      spellLevel1: ["Slot 1", "Slot 2", "Slot 3", "Slot 4"],
+      spellLevel2: ["Slot 1", "Slot 2", "Slot 3"],
+      spellLevel3: ["Slot 1", "Slot 2", "Slot 3"],
+      spellLevel4: ["Slot 1", "Slot 2", "Slot 3"],
+      spellLevel5: ["Slot 1", "Slot 2"],
+      spellLevel6: ["Slot 1"],
+      spellLevel7: ["Slot 1"],
+      spellLevel8: ["Slot 1"],
+      spellLevel9: ["Slot 1"]
     },
     {
       classLevel: 18,
-      spellLevel1: 4,
-      spellLevel2: 3,
-      spellLevel3: 3,
-      spellLevel4: 3,
-      spellLevel5: 3,
-      spellLevel6: 1,
-      spellLevel7: 1,
-      spellLevel8: 1,
-      spellLevel9: 1
+      spellLevel1: ["Slot 1", "Slot 2", "Slot 3", "Slot 4"],
+      spellLevel2: ["Slot 1", "Slot 2", "Slot 3"],
+      spellLevel3: ["Slot 1", "Slot 2", "Slot 3"],
+      spellLevel4: ["Slot 1", "Slot 2", "Slot 3"],
+      spellLevel5: ["Slot 1", "Slot 2", "Slot 3"],
+      spellLevel6: ["Slot 1"],
+      spellLevel7: ["Slot 1"],
+      spellLevel8: ["Slot 1"],
+      spellLevel9: ["Slot 1"]
     },
     {
       classLevel: 19,
-      spellLevel1: 4,
-      spellLevel2: 3,
-      spellLevel3: 3,
-      spellLevel4: 3,
-      spellLevel5: 3,
-      spellLevel6: 2,
-      spellLevel7: 1,
-      spellLevel8: 1,
-      spellLevel9: 1
+      spellLevel1: ["Slot 1", "Slot 2", "Slot 3", "Slot 4"],
+      spellLevel2: ["Slot 1", "Slot 2", "Slot 3"],
+      spellLevel3: ["Slot 1", "Slot 2", "Slot 3"],
+      spellLevel4: ["Slot 1", "Slot 2", "Slot 3"],
+      spellLevel5: ["Slot 1", "Slot 2", "Slot 3"],
+      spellLevel6: ["Slot 1", "Slot 2"],
+      spellLevel7: ["Slot 1"],
+      spellLevel8: ["Slot 1"],
+      spellLevel9: ["Slot 1"]
     },
     {
       classLevel: 20,
-      spellLevel1: 4,
-      spellLevel2: 3,
-      spellLevel3: 3,
-      spellLevel4: 3,
-      spellLevel5: 3,
-      spellLevel6: 2,
-      spellLevel7: 2,
-      spellLevel8: 1,
-      spellLevel9: 1
+      spellLevel1: ["Slot 1", "Slot 2", "Slot 3", "Slot 4"],
+      spellLevel2: ["Slot 1", "Slot 2", "Slot 3"],
+      spellLevel3: ["Slot 1", "Slot 2", "Slot 3"],
+      spellLevel4: ["Slot 1", "Slot 2", "Slot 3"],
+      spellLevel5: ["Slot 1", "Slot 2", "Slot 3"],
+      spellLevel6: ["Slot 1", "Slot 2"],
+      spellLevel7: ["Slot 1", "Slot 2"],
+      spellLevel8: ["Slot 1"],
+      spellLevel9: ["Slot 1"]
     },
   ]
   public casterLevel: number = 1;
-
-  Arr = Array;
-  num:number = 0;
 }
